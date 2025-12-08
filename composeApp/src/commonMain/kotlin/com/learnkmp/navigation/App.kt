2ea66e5ce6dcc6b036ddfe34548bd4f761e5aa20
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-12-08 18:44:24 +0700

    Finished setting up

--- composeApp/src/commonMain/kotlin/com/learnkmp/navigation/App.kt
+++ composeApp/src/commonMain/kotlin/com/learnkmp/navigation/App.kt
@@ -3,18 +3,21 @@ package com.learnkmp.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.learnkmp.navigation.todo.TaskDetailScreen
 import com.learnkmp.navigation.todo.TaskListScreen
 
-data object TaskListKey
-data class TaskDetailsKey(val taskId: Int)
+//Make serializable in case you want to preserve during state change
+data object TaskListKey: NavKey
+
+data class TaskDetailsKey(val taskId: Int): NavKey
 
 @Composable
 fun App() {
 
-    val backStack = remember { mutableStateListOf<Any>(TaskListKey) }
+    val backStack = remember { mutableStateListOf<NavKey>(TaskListKey) }
 
     NavDisplay(
         backStack = backStack,
