5d2d2588ce2fe98515b72df8ec0cdf8d98643f43
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-12-08 19:18:10 +0700

    Fixed dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -26,19 +26,17 @@ kotlin {
     
     sourceSets {
         androidMain.dependencies {
-            implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
+            implementation(libs.runtime)
+            implementation(libs.foundation)
+            implementation(libs.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.androidx.nav3.ui)
             implementation(libs.androidx.core.icons)
         }
         commonTest.dependencies {
