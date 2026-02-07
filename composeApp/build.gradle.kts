f5399bb91593a47de00dbb54a7403fa96b64f2aa
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-02-07 20:05:47 +0700

    Updated dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,12 +29,8 @@ kotlin {
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.material3)
+            implementation(libs.ui)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.androidx.core.icons)
